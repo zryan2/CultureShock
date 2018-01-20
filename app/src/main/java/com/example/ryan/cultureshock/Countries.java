69bd3a85acef6b45b0d227507328a7823e0064cc
Ryan Zeng <zeng.ryan2@gmail.com>
2018-01-20 11:25:04 -0500

    Added new Country Screen Changed new starting activity

---
+++ app/src/main/java/com/example/ryan/cultureshock/Countries.java
@@ -0,0 +1,13 @@
+package com.example.ryan.cultureshock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Countries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countries);
+    }
+}
