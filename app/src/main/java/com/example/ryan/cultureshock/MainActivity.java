57d050c153652f1149cbe1fe5b4c039ca1614295
Marco Austria <maustria0507@yahoo.com>
2018-01-21 03:33:05 -0500

    deleted expandable list, uncommented default, runs well

--- app/src/main/java/com/example/ryan/cultureshock/MainActivity.java
+++ app/src/main/java/com/example/ryan/cultureshock/MainActivity.java
@@ -31,11 +31,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     Fragment selectedFragment = null;
 
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -87,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
                     return true;
                 case R.id.navigation_feed:
-                    //go to phrases fragment
+                    //go to feed fragment
                     Comments comments = new Comments();
                     FragmentTransaction transaction5 = getSupportFragmentManager().beginTransaction();
                     selectedFragment = comments;
@@ -106,21 +101,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        //EXPANDABLE LIST
-//        // get the listview
-//        expListView = (ExpandableListView) findViewById(R.id.ELV);
-//        // preparing list data
-//        prepareListData();
-//        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-//        // setting list adapter
-//        expListView.setAdapter(listAdapter);
-//
-//        //default info after click country
-//        CountryInfo countryInfo = new CountryInfo();
-//        FragmentTransaction transaction4 = getSupportFragmentManager().beginTransaction();
-//        selectedFragment = countryInfo;
-//        transaction4.replace(R.id.contentLayout, selectedFragment);
-//        transaction4.commit();
+        //default info after click country
+        CountryInfo countryInfo = new CountryInfo();
+        FragmentTransaction transaction4 = getSupportFragmentManager().beginTransaction();
+        selectedFragment = countryInfo;
+        transaction4.replace(R.id.contentLayout, selectedFragment);
+        transaction4.commit();
 
 
 
@@ -138,48 +124,5 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationViewHelper.disableShiftMode(navigation);
 
     }
-
-//    /*
-//     * Preparing the list data
-//     */
-//    private void prepareListData() {
-//        listDataHeader = new ArrayList<String>();
-//        listDataChild = new HashMap<String, List<String>>();
-//
-//        // Adding child data
-//        listDataHeader.add("Top 250");
-//        listDataHeader.add("Now Showing");
-//        listDataHeader.add("Coming Soon..");
-//
-//        // Adding child data
-//        List<String> top250 = new ArrayList<String>();
-//        top250.add("The Shawshank Redemption");
-//        top250.add("The Godfather");
-//        top250.add("The Godfather: Part II");
-//        top250.add("Pulp Fiction");
-//        top250.add("The Good, the Bad and the Ugly");
-//        top250.add("The Dark Knight");
-//        top250.add("12 Angry Men");
-//
-//        List<String> nowShowing = new ArrayList<String>();
-//        nowShowing.add("The Conjuring");
-//        nowShowing.add("Despicable Me 2");
-//        nowShowing.add("Turbo");
-//        nowShowing.add("Grown Ups 2");
-//        nowShowing.add("Red 2");
-//        nowShowing.add("The Wolverine");
-//
-//        List<String> comingSoon = new ArrayList<String>();
-//        comingSoon.add("2 Guns");
-//        comingSoon.add("The Smurfs 2");
-//        comingSoon.add("The Spectacular Now");
-//        comingSoon.add("The Canyons");
-//        comingSoon.add("Europa Report");
-//
-//        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-//        listDataChild.put(listDataHeader.get(1), nowShowing);
-//        listDataChild.put(listDataHeader.get(2), comingSoon);
-//    }
-
 
 }
